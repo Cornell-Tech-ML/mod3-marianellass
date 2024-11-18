a5c4c42a527edca4cf606d1b2af3f02a580ef13e
marianellas <se.marianella@gmail.com>
2024-11-17 23:28:38 -0500

    test 3.1

--- minitorch/autodiff.py
+++ minitorch/autodiff.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import Any, Iterable, List, Tuple, Protocol
+from typing import Any, Iterable, Tuple, Protocol
 
 
 # ## Task 1.1
